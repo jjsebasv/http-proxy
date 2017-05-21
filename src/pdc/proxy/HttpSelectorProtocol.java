88c1a31da2dd430f3af7d7791c521a61ea141997
heyimnowi <noelia.lopez@mercadolibre.com>
2017-05-21 13:45:48 -0300

    Http proxy

---
+++ src/pdc/proxy/HttpSelectorProtocol.java
@@ -0,0 +1,45 @@
+package pdc.proxy;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+import nio.TCPProtocol;
+
+public class HttpSelectorProtocol implements TCPProtocol {
+	private int bufferSize;
+
+	public HttpSelectorProtocol(int bufferSize) {
+		this.bufferSize = bufferSize;
+	}
+
+	public void handleAccept(SelectionKey key) throws IOException {
+		SocketChannel clientChannel = ((ServerSocketChannel) key.channel()).accept();
+		clientChannel.configureBlocking(false);
+		clientChannel.register(key.selector(), SelectionKey.OP_READ, ByteBuffer.allocate(bufferSize));
+	}
+
+	public void handleRead(SelectionKey key) throws IOException {
+		SocketChannel clientChannel = (SocketChannel) key.channel();
+		ByteBuffer buffer = (ByteBuffer) key.attachment();
+		long bytesRead = clientChannel.read(buffer);
+		if (bytesRead == -1) {
+			clientChannel.close();
+		} else if (bytesRead > 0) {
+			key.interestOps(SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+		}
+	}
+
+	public void handleWrite(SelectionKey key) throws IOException {
+		ByteBuffer buffer = (ByteBuffer) key.attachment();
+		buffer.flip();
+		SocketChannel clientChannel = (SocketChannel) key.channel();
+		clientChannel.write(buffer);
+		if (!buffer.hasRemaining()) {
+			key.interestOps(SelectionKey.OP_READ);
+		}
+		buffer.compact();
+	}
+}
---
+++ src/pdc/proxy/HttpServerSelector.java
@@ -0,0 +1,57 @@
+package pdc.proxy;
+
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.util.Iterator;
+
+import nio.TCPProtocol;
+
+
+public class HttpServerSelector {
+    private static final int BUFSIZE = 256;
+    private static final int TIMEOUT = 3000;
+    private static final int PORT = 9090;
+
+    public static void main(String[] args) throws IOException {
+
+        Selector selector = Selector.open();
+
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.socket().bind(new InetSocketAddress(PORT));
+        serverSocketChannel.configureBlocking(false);
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            
+        TCPProtocol protocol = new HttpSelectorProtocol(BUFSIZE);
+
+        while (true) {
+
+        	if (selector.select(TIMEOUT) == 0) {
+                System.out.print(".");
+                continue;
+            }
+
+        	Iterator<SelectionKey> keyIter = selector.selectedKeys().iterator();
+        	
+            while (keyIter.hasNext()) {
+                SelectionKey key = keyIter.next();
+
+                if (key.isAcceptable()) {
+                    protocol.handleAccept(key);
+                }
+
+                if (key.isReadable()) {
+                    protocol.handleRead(key);
+                }
+
+                if (key.isValid() && key.isWritable()) {
+                    protocol.handleWrite(key);
+                }
+                keyIter.remove();
+            }
+        }
+    }
+}
